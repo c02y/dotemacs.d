34fca39d81c060874fac3069a74a19e05262018c
codychan <cody.chan.cz@gmail.com>
2014-03-16 03:53:13 +0800

    update in packages

---
+++ elpa/expand-region-20140314.1406/expand-region-pkg.el
@@ -0,0 +1,1 @@
+(define-package "expand-region" "20140314.1406" "Increase selected region by semantic units." 'nil)
--- elpa/helm-20140312.225/helm-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "helm" "20140312.225" "Helm is an Emacs incremental and narrowing framework" 'nil)
