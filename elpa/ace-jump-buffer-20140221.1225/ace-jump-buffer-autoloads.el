ce50fe4998400fda8758786095917f5165573178
codychan <cody.chan.cz@gmail.com>
2014-03-20 17:18:38 +0800

    remove slime and other updates

---
+++ elpa/ace-jump-buffer-20140221.1225/ace-jump-buffer-autoloads.el
@@ -0,0 +1,29 @@
+;;; ace-jump-buffer-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+
+;;;### (autoloads (ace-jump-buffer) "ace-jump-buffer" "ace-jump-buffer.el"
+;;;;;;  (21289 39664 725182 990000))
+;;; Generated autoloads from ace-jump-buffer.el
+
+(autoload 'ace-jump-buffer "ace-jump-buffer" "\
+Quickly hop between buffers with `ace-jump-mode'
+
+\(fn)" t nil)
+
+;;;***
+
+;;;### (autoloads nil nil ("ace-jump-buffer-pkg.el") (21289 39664
+;;;;;;  854773 140000))
+
+;;;***
+
+(provide 'ace-jump-buffer-autoloads)
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; ace-jump-buffer-autoloads.el ends here
--- elpa/expand-region-20140314.1406/expand-region-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "expand-region" "20140314.1406" "Increase selected region by semantic units." 'nil)
