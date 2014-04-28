62df53871e04080a4c097dbd3ab0c667345b6579
codychan <cody.chan.cz@gmail.com>
2014-04-28 18:17:48 +0800

    updates in packages

---
+++ elpa/evil-20140426.1337/evil-pkg.el
@@ -0,0 +1,3 @@
+(define-package "evil" "20140426.1337" "Extensible Vi layer for Emacs."
+  '((undo-tree "0.6.3")
+    (goto-chg "1.6")))
---
+++ elpa/helm-20140425.651/helm-pkg.el
@@ -0,0 +1,1 @@
+(define-package "helm" "20140425.651" "Helm is an Emacs incremental and narrowing framework" 'nil)
