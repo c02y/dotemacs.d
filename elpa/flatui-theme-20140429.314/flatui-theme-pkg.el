52b9c2dd6401e5ab30c0420d55c3aa451ab02550
codychan <cody.chan.cz@gmail.com>
2014-05-06 22:26:44 +0800

    updates in packages

---
+++ elpa/flatui-theme-20140429.314/flatui-theme-pkg.el
@@ -0,0 +1,1 @@
+(define-package "flatui-theme" "20140429.314" "A color theme for Emacs based on flatuicolors.com" (quote nil))
--- elpa/ggtags-20140423.2006/ggtags-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "ggtags" "20140423.2006" "emacs frontend to GNU Global source code tagging system" (quote ((emacs "24") (cl-lib "0.5"))))
