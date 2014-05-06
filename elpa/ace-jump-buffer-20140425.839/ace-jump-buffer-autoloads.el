b76d3a32a53adad0d55bc55ea45ff8d2539cb213
codychan <cody.chan.cz@gmail.com>
2014-05-07 01:03:54 +0800

    delete the "listp, <<<<<<<" errors

--- elpa/ace-jump-buffer-20140425.839/ace-jump-buffer-autoloads.el
+++ elpa/ace-jump-buffer-20140425.839/ace-jump-buffer-autoloads.el
@@ -3,43 +3,6 @@
 ;;; Code:
 
 
-;;;### (autoloads (ace-jump-buffer-in-one-window ace-jump-buffer-other-window
-<<<<<<< HEAD
-;;;;;;  ace-jump-buffer) "ace-jump-buffer" "ace-jump-buffer.el" (21342
-;;;;;;  9731 422841 226000))
-=======
-;;;;;;  ace-jump-buffer) "ace-jump-buffer" "ace-jump-buffer.el" (21343
-;;;;;;  58206 948107 16000))
->>>>>>> 5fe63e4944dc2ce248c18d0c179c43470731b8c7
-;;; Generated autoloads from ace-jump-buffer.el
-
-(autoload 'ace-jump-buffer "ace-jump-buffer" "\
-Quickly hop to buffer with `ace-jump-mode'.
-
-\(fn)" t nil)
-
-(autoload 'ace-jump-buffer-other-window "ace-jump-buffer" "\
-Quickly hop to buffer with `ace-jump-mode' in other window.
-
-\(fn)" t nil)
-
-(autoload 'ace-jump-buffer-in-one-window "ace-jump-buffer" "\
-Quickly hop to buffer with `ace-jump-mode' in one window.
-
-\(fn)" t nil)
-
-;;;***
-
-<<<<<<< HEAD
-;;;### (autoloads nil nil ("ace-jump-buffer-pkg.el") (21342 9731
-;;;;;;  493486 454000))
-=======
-;;;### (autoloads nil nil ("ace-jump-buffer-pkg.el") (21343 58207
-;;;;;;  60242 895000))
->>>>>>> 5fe63e4944dc2ce248c18d0c179c43470731b8c7
-
-;;;***
-
 (provide 'ace-jump-buffer-autoloads)
 ;; Local Variables:
 ;; version-control: never
--- elpa/yasnippet-20140427.1224/yasnippet-autoloads.el
+++ elpa/yasnippet-20140427.1224/yasnippet-autoloads.el
@@ -3,63 +3,6 @@
 ;;; Code:
 
 
-;;;### (autoloads (yas-global-mode yas-minor-mode) "yasnippet" "yasnippet.el"
-<<<<<<< HEAD:elpa/yasnippet-20140427.1224/yasnippet-autoloads.el
-;;;;;;  (21342 9678 562579 107000))
-=======
-;;;;;;  (21343 58134 271113 99000))
->>>>>>> 5fe63e4944dc2ce248c18d0c179c43470731b8c7:elpa/yasnippet-20140427.1224/yasnippet-autoloads.el
-;;; Generated autoloads from yasnippet.el
-
-(autoload 'yas-minor-mode "yasnippet" "\
-Toggle YASnippet mode.
-
-When YASnippet mode is enabled, `yas-expand', normally bound to
-the TAB key, expands snippets of code depending on the major
-mode.
-
-With no argument, this command toggles the mode.
-positive prefix argument turns on the mode.
-Negative prefix argument turns off the mode.
-
-Key bindings:
-\\{yas-minor-mode-map}
-
-\(fn &optional ARG)" t nil)
-
-(defvar yas-global-mode nil "\
-Non-nil if Yas-Global mode is enabled.
-See the command `yas-global-mode' for a description of this minor mode.
-Setting this variable directly does not take effect;
-either customize it (see the info node `Easy Customization')
-or call the function `yas-global-mode'.")
-
-(custom-autoload 'yas-global-mode "yasnippet" nil)
-
-(autoload 'yas-global-mode "yasnippet" "\
-Toggle Yas minor mode in all buffers.
-With prefix ARG, enable Yas-Global mode if ARG is positive;
-otherwise, disable it.  If called from Lisp, enable the mode if
-ARG is omitted or nil.
-
-Yas minor mode is enabled in all buffers where
-`yas-minor-mode-on' would do it.
-See `yas-minor-mode' for more information on Yas minor mode.
-
-\(fn &optional ARG)" t nil)
-
-;;;***
-
-<<<<<<< HEAD:elpa/yasnippet-20140427.1224/yasnippet-autoloads.el
-;;;### (autoloads nil nil ("yasnippet-pkg.el") (21342 9678 724395
-;;;;;;  918000))
-=======
-;;;### (autoloads nil nil ("yasnippet-pkg.el") (21343 58134 357380
-;;;;;;  459000))
->>>>>>> 5fe63e4944dc2ce248c18d0c179c43470731b8c7:elpa/yasnippet-20140427.1224/yasnippet-autoloads.el
-
-;;;***
-
 (provide 'yasnippet-autoloads)
 ;; Local Variables:
 ;; version-control: never
