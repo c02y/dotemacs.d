3bd3a37890c8cc9d0e4ef38637b8f2517f68b6d3
codychan <cody.chan.cz@gmail.com>
2014-06-24 15:29:16 +0800

    updates in packages

---
+++ elpa/helm-20140623.2213/helm-pkg.el
@@ -0,0 +1,1 @@
+(define-package "helm" "20140623.2213" "Helm is an Emacs incremental and narrowing framework" 'nil)
---
+++ elpa/org-plus-contrib-20140623/org-plus-contrib-pkg.el
@@ -0,0 +1,3 @@
+(define-package "org-plus-contrib"
+  "20140623" "Outline-based notes management and organizer" ( ))
+;; no-byte-compile: t
