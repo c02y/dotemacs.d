f7c433b9db313723f63b4776d0e33c16d3457600
codychan <cody.chan.cz@gmail.com>
2014-06-27 11:55:48 +0800

    install workgroups2 and updates in pakcages

---
+++ elpa/workgroups2-20140625.1044/workgroups2-pkg.el
@@ -0,0 +1,4 @@
+(define-package "workgroups2" "20140625.1044" "New workspaces for Emacs"
+  '((cl-lib "0.4"))
+  :url "https://github.com/pashinin/workgroups2" :keywords
+  '("session" "management" "window-configuration" "persistence"))
