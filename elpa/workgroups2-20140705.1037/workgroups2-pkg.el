4e80411ce1b7901791a5d20f81f1940be3f68b64
codychan <cody.chan.cz@gmail.com>
2014-07-25 23:10:37 +0800

    updates in packages

--- elpa/workgroups2-20140705.1037/workgroups2-pkg.el
+++
@@ -1,4 +0,0 @@
-(define-package "workgroups2" "20140705.1037" "New workspaces for Emacs"
-  '((cl-lib "0.4"))
-  :url "https://github.com/pashinin/workgroups2" :keywords
-  '("session" "management" "window-configuration" "persistence"))
---
+++ elpa/workgroups2-20140719.2330/workgroups2-pkg.el
@@ -0,0 +1,4 @@
+(define-package "workgroups2" "20140719.2330" "New workspaces for Emacs"
+  '((cl-lib "0.4"))
+  :url "https://github.com/pashinin/workgroups2" :keywords
+  '("session" "management" "window-configuration" "persistence"))
