12bc493b436331b471490d43561c01021382dc39
codychan <cody.chan.cz@gmail.com>
2014-08-06 07:15:55 +0800

    updates and delete *projectile* packages

--- elpa/evil-20140625.411/evil-autoloads.el
+++
@@ -1,28 +0,0 @@
-;;; evil-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-
-
-;;;### (autoloads nil "evil-core" "evil-core.el" (21439 48873 540246
-;;;;;;  357000))
-;;; Generated autoloads from evil-core.el
- (autoload 'evil-mode "evil" "Toggle evil in all buffers" t)
-
-;;;***
-
-;;;### (autoloads nil nil ("evil-commands.el" "evil-common.el" "evil-digraphs.el"
-;;;;;;  "evil-ex.el" "evil-integration.el" "evil-macros.el" "evil-maps.el"
-;;;;;;  "evil-pkg.el" "evil-repeat.el" "evil-search.el" "evil-states.el"
-;;;;;;  "evil-types.el" "evil-vars.el" "evil.el") (21439 48873 665710
-;;;;;;  208000))
-
-;;;***
-
-(provide 'evil-autoloads)
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; coding: utf-8
-;; End:
-;;; evil-autoloads.el ends here
---
+++ elpa/org-plus-contrib-20140804/org-plus-contrib-pkg.el
@@ -0,0 +1,3 @@
+(define-package "org-plus-contrib"
+  "20140804" "Outline-based notes management and organizer" ( ))
+;; no-byte-compile: t
