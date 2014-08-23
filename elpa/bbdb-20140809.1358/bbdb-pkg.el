a71377cd5e858b033e0c02d03022a7e922bfa682
codychan <cody.chan.cz@gmail.com>
2014-08-23 08:55:40 +0800

    updates and install packages needed by gnus

---
+++ elpa/bbdb-20140809.1358/bbdb-pkg.el
@@ -0,0 +1,1 @@
+(define-package "bbdb" "20140809.1358" "The Insidious Big Brother Database for GNU Emacs" 'nil)
--- elpa/epl-20140820.436/epl-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "epl" "20140820.436" "Emacs Package Library" (quote ((cl-lib "0.3"))))
