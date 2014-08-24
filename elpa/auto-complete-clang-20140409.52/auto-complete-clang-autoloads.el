8851d08b86fef6a847f45191f9048a4f81f474dd
codychan <cody.chan.cz@gmail.com>
2014-08-25 00:56:14 +0800

    updates in packages

---
+++ elpa/auto-complete-clang-20140409.52/auto-complete-clang-autoloads.el
@@ -0,0 +1,18 @@
+;;; auto-complete-clang-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+
+;;;### (autoloads nil nil ("auto-complete-clang-pkg.el" "auto-complete-clang.el")
+;;;;;;  (21497 46506 876862 247000))
+
+;;;***
+
+(provide 'auto-complete-clang-autoloads)
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; auto-complete-clang-autoloads.el ends here
