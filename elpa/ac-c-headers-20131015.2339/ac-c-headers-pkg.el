bbdfaf8a825464336046761968592db4cdd081e0
codychan <cody.chan.cz@gmail.com>
2014-08-31 12:29:36 +0800

    replace auto-complete with company-mode totally, ac is laggy and too much to config

--- elpa/ac-c-headers-20131015.2339/ac-c-headers-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "ac-c-headers" "20131015.2339" "auto-complete source for C headers" (quote nil))
--- elpa/fuzzy-20131025.2343/fuzzy-autoloads.el
+++
@@ -1,18 +0,0 @@
-;;; fuzzy-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-
-
-;;;### (autoloads nil nil ("fuzzy-pkg.el" "fuzzy.el") (21183 59159
-;;;;;;  838634 482000))
-
-;;;***
-
-(provide 'fuzzy-autoloads)
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; coding: utf-8
-;; End:
-;;; fuzzy-autoloads.el ends here
