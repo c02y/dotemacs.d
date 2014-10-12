b7c52fd9a9cfcb4663b8a742816811a8c9d7079d
codychan <cody.chan.cz@gmail.com>
2014-10-13 06:52:31 +0800

    elpa: updates

---
+++ elpa/yafolding-20141011.112/yafolding-autoloads.el
@@ -0,0 +1,31 @@
+;;; yafolding-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+
+;;;### (autoloads (yafolding-mode) "yafolding" "yafolding.el" (21563
+;;;;;;  1134 767935 928000))
+;;; Generated autoloads from yafolding.el
+
+(defvar yafolding-mode-map (let ((map (make-sparse-keymap))) (define-key map (kbd "<C-S-return>") #'yafolding-hide-parent-element) (define-key map (kbd "<C-M-return>") #'yafolding-toggle-all) (define-key map (kbd "<C-return>") #'yafolding-toggle-element) map))
+
+(autoload 'yafolding-mode "yafolding" "\
+Toggle yafolding mode.
+
+\(fn &optional ARG)" t nil)
+
+;;;***
+
+;;;### (autoloads nil nil ("yafolding-pkg.el") (21563 1134 849575
+;;;;;;  377000))
+
+;;;***
+
+(provide 'yafolding-autoloads)
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; yafolding-autoloads.el ends here
---
+++ elpa/yafolding-20141011.112/yafolding-pkg.el
@@ -0,0 +1,1 @@
+(define-package "yafolding" "20141011.112" "Yet another folding extension for Emacs" (quote nil))
