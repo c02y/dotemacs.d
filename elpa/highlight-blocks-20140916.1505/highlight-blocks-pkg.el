85c05f01fa4afef27f4cff22d0067bd1eeb43680
codychan <cody.chan.cz@gmail.com>
2014-10-20 22:36:42 +0800

    elpa: add comment-dwim-2, updates

--- elpa/highlight-blocks-20140916.1505/highlight-blocks-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "highlight-blocks" "20140916.1505" "Highlight the blocks point is in" (quote ((emacs "24"))))
---
+++ elpa/highlight-blocks-20141019.500/highlight-blocks-pkg.el
@@ -0,0 +1,1 @@
+(define-package "highlight-blocks" "20141019.500" "Highlight the blocks point is in" (quote ((emacs "24"))))
