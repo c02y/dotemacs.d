2eb19af5f0abae7c15bdeaf3ea15a495b7e76ab1
codychan <cody.chan.cz@gmail.com>
2014-10-31 11:38:01 +0800

    elpa: add fish-mode for editing fish-shell related files, also work for bash

---
+++ elpa/fish-mode-20141026.1934/fish-mode-autoloads.el
@@ -0,0 +1,31 @@
+;;; fish-mode-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+
+;;;### (autoloads (fish-mode) "fish-mode" "fish-mode.el" (21586 3138
+;;;;;;  594495 311000))
+;;; Generated autoloads from fish-mode.el
+
+(autoload 'fish-mode "fish-mode" "\
+Major mode for editing fish shell files.
+
+\(fn)" t nil)
+ (add-to-list 'auto-mode-alist '("\\.fish\\'" . fish-mode))
+ (add-to-list 'interpreter-mode-alist '("fish" . fish-mode))
+
+;;;***
+
+;;;### (autoloads nil nil ("fish-mode-pkg.el") (21586 3138 678686
+;;;;;;  112000))
+
+;;;***
+
+(provide 'fish-mode-autoloads)
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; fish-mode-autoloads.el ends here
---
+++ elpa/fish-mode-20141026.1934/fish-mode-pkg.el
@@ -0,0 +1,1 @@
+(define-package "fish-mode" "20141026.1934" "Major mode for fish shell scripts" (quote ((emacs "24"))))
