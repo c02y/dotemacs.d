c0d603a092dcb2ef6b4d4694ec12101613c3bebc
c02y <cody.chan.cz@gmail.com>
2014-11-30 20:02:46 +0800

    elpa: add lispy(require iedit and noflet) package, updates

--- elpa/ggtags-20141112.2234/ggtags-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "ggtags" "20141112.2234" "emacs frontend to GNU Global source code tagging system" (quote ((emacs "24") (cl-lib "0.5"))))
---
+++ elpa/ggtags-20141129.2333/ggtags-pkg.el
@@ -0,0 +1,1 @@
+(define-package "ggtags" "20141129.2333" "emacs frontend to GNU Global source code tagging system" (quote ((emacs "24") (cl-lib "0.5"))))
