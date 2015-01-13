bb7638ea1e58338e9a178bf0720fa04960e492b9
c02y <cody.chan.cz@gmail.com>
2015-01-13 19:19:45 +0800

    elpa: replace lisp/smooth-scroll.el with elpa/smooth-scroll-20130321, updates

--- elpa/modeline-posn-20150105.28/modeline-posn-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "modeline-posn" "20150105.28" "Set up `mode-line-position'." 'nil :url "http://www.emacswiki.org/modeline-posn.el" :keywords '("mode-line" "region" "column"))
---
+++ elpa/modeline-posn-20150109.1541/modeline-posn-pkg.el
@@ -0,0 +1,1 @@
+(define-package "modeline-posn" "20150109.1541" "Set up `mode-line-position'." 'nil :url "http://www.emacswiki.org/modeline-posn.el" :keywords '("mode-line" "region" "column"))
