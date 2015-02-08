d5793cd86a42eab02fad6d57543a1aee0e3b08d6
c02y <cody.chan.cz@gmail.com>
2015-02-09 05:54:12 +0800

    elpa: updates

---
+++ elpa/moe-theme-20150203.157/moe-theme-autoloads.el
@@ -0,0 +1,17 @@
+;;; moe-theme-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
+
+;;;### (autoloads nil nil ("moe-dark-theme.el" "moe-light-theme.el"
+;;;;;;  "moe-theme-pkg.el" "moe-theme-switcher.el" "moe-theme.el")
+;;;;;;  (21714 12929 886836 90000))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; moe-theme-autoloads.el ends here
