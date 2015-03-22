f87aa8384d45ab1219279801d0a03bd49813fbfd
c02y <cody.chan.cz@gmail.com>
2015-03-22 19:26:11 +0800

    elpa: updates

---
+++ elpa/hydra-20150321.1130/hydra-pkg.el
@@ -0,0 +1,7 @@
+(define-package "hydra" "20150321.1130" "Make bindings that stick around"
+  '((cl-lib "0.5"))
+  :url "https://github.com/abo-abo/hydra" :keywords
+  '("bindings"))
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
---
+++ elpa/lispy-20150319.530/lispy-pkg.el
@@ -0,0 +1,13 @@
+(define-package "lispy" "20150319.530" "vi-like Paredit"
+  '((emacs "24.1")
+    (helm "1.5.3")
+    (ace-jump-mode "2.0")
+    (s "1.4.0")
+    (multiple-cursors "1.3.0")
+    (iedit "0.97")
+    (highlight "0")
+    (hydra "0.4.1")
+    (ace-window "0.7.0")))
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
