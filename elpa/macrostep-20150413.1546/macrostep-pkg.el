7fabf512767a1fe863f0138c985c4c778e505da1
c02y <cody.chan.cz@gmail.com>
2015-04-19 18:23:18 +0800

    elpa: add nasm-mode to replace lisp/nasm-mode.el and updates

--- elpa/macrostep-20150413.1546/macrostep-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "macrostep" "20150413.1546" "interactive macro stepper for Emacs Lisp" 'nil :url "https://github.com/joddie/macrostep" :keywords '("lisp" "languages" "macro" "debugging"))
