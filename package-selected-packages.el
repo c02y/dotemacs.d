8fd1181fd4b0ca37978c712f88c828340dac677a
c02y <cody.chan.cz@gmail.com>
2016-07-24 01:24:19 +0800

    new file for package-selected-packages variable

---
+++ package-selected-packages.el
@@ -0,0 +1,14 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   (quote
+	(magit zeal-at-point yasnippet yafolding xcscope whole-line-or-region which-key web-mode w3m vlf undo-tree tagedit syntax-subword stickyfunc-enhance speed-type smooth-scroll sml-mode smartparens slime restart-emacs rebox2 ranger rainbow-mode rainbow-identifiers rainbow-delimiters racket-mode paradox org-plus-contrib org omni-scratch nasm-mode names multiple-cursors molokai-theme moe-theme modeline-posn markdown-mode+ lua-mode lispy iy-go-to-char ido-vertical-mode hippie-exp-ext highlight-symbol highlight-parentheses highlight-indentation highlight hide-comnt helm-swoop helm-projectile helm-descbinds helm-ag go-eldoc ggtags geiser flycheck-pos-tip flatui-theme fish-mode findr f esup emmet-mode ecb dropdown-list drag-stuff discover-my-major dired+ deferred company-go company-c-headers comment-dwim-2 color-identifiers-mode change-inner c-eldoc bookmark+ bind-key bbyac bbdb base16-theme ascope anchored-transpose afternoon-theme ace-jump-mode ace-jump-buffer))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
