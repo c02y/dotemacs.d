76624fc757d82b9159d04bc49c213ee9bcae858b
c02y <cody.chan.cz@gmail.com>
2018-03-28 22:50:37 +0800

    init.el: move highlight-beyond-fill-column from emacs-goodies-el system package into .emacs.d/lisp, improve the default-frame-alist, add copy-* functions, add xsh-syntax-color-tex function, improve iPython configs, move tex latated to .emacs.d/lisp/init-tex.el

--- init.el
+++ init.el
@@ -266,6 +266,7 @@ and you can reconfigure the compile args."
 (setq bookmark-save-flag t)
 (setq column-number-mode t)
 (setq-default fill-column 80)
+(require 'highlight-beyond-fill-column)
 (add-hook 'prog-mode-hook 'highlight-beyond-fill-column)
 
 ;; highlight links in any mode
@@ -422,25 +423,18 @@ and you can reconfigure the compile args."
   (interactive)
   (if window-system
 	  (progn
-		;; use 120 char wide window for largish displays
-		;; and smaller 80 column windows for smaller displays
-		;; pick whatever numbers make sense for you
+		(setq width-chars (/ (/ (x-display-pixel-width) (frame-char-width)) 2))
+		(setq height-lines (- (/ (x-display-pixel-height) (frame-char-height)) 3))
+		(setq default-frame-alist
+			  `((top . 0) (left . 0)
+				(width . ,width-chars)
+				(height . ,height-lines)))
 		(if (> (x-display-pixel-width) 1500)
-			;; (add-to-list 'default-frame-alist (cons 'width 85))
-			;; (add-to-list 'default-frame-alist (cons 'width 85)))
-			(setq default-frame-alist
-				  '((top . 0) (left . 0)
-					(width . 95) (height . 48)
-					;; or Monaco, Bitstream Vera Sans Mono, Liberation Mono
-					;; (font . "Input Mono Compressed-14")))
-					(font . "PragmataPro-14")))
-		  (setq default-frame-alist
-				'((top . 0) (left . 0)
-				  (width . 85) (height . 37)
-				  ;; (font . "Input Mono Compressed-13.5")
-				  (font . "PragmataPro-13")
-				  ;; (:family "Menlo-Italic")
-				  )))))
+			;; or Monaco, Bitstream Vera Sans Mono, Liberation Mono
+			;; (add-to-list 'default-frame-alist '(font . "PragmataPro-14"))
+			(add-to-list 'default-frame-alist '(font . "Input Mono Compressed-14"))
+		  (add-to-list 'default-frame-alist '(font . "Input Mono Compressed-13")))
+		))
   ;; the following two settings are specifically for afternoon-theme
   ;; the combination colors of highlighted line and comments
   ;; (custom-set-faces
@@ -886,13 +880,15 @@ With negative N, comment out original line and use the absolute value."
 	  (kill-new filename)
 	  (message "'%s' name copied!" filename))))
 (defun copy-path-short ()
-  "Copy the path (using ~) of current buffer file to the clipboard."
+  "Copy the path of current buffer file to the clipboard."
   (interactive)
   (let ((filename (if (equal major-mode 'dired-mode)
 					  default-directory
 					;; abbreviate-file-name will replace /home/user with ~
 					;; also works with directory
-					(abbreviate-file-name buffer-file-name))))
+					(if buffer-file-name
+						(abbreviate-file-name buffer-file-name)
+					  (user-error "Current buffer is not a file/directory in disk!" "exit")))))
 	(when filename
 	  (kill-new filename)
 	  (message "'%s' path copied!" filename))))
@@ -902,9 +898,47 @@ With negative N, comment out original line and use the absolute value."
   (let ((filename (if (equal major-mode 'dired-mode)
 					  default-directory
 					(buffer-file-name))))
-	(when filename
-	  (kill-new filename)
-	  (message "'%s' path copied!" filename))))
+	(if filename
+		(progn
+		  (kill-new filename)
+		  (message "'%s' path copied!" filename))
+	  (message "Current buffer is not a file/directory in disk!"))))
+(defun copy-path-html ()
+  "Copy the html of according to the current buffer, this is useful when exporting org file to html."
+  (interactive)
+  (let ((filename (if (equal major-mode 'dired-mode)
+  					  default-directory
+  					(buffer-file-name))))
+  	(if filename
+  		(progn
+  		  (kill-new filename)
+  		  (message "'%s' path copied!" filename))
+	  ;; print error message and exit/return the function
+  	  (user-error "Current buffer is not a file in disk!" "exit")))
+  (let ((html-file (concat (file-name-sans-extension (buffer-file-name)) ".html")))
+	(if (file-exists-p html-file)
+		(progn
+		  (kill-new html-file)
+		  (message "'%s' path copied!" html-file))
+	  (message "'%s' is not a file in disk!" html-file))))
+(defun copy-path-pdf ()
+  "Copy the pdf of according to the current buffer, this is useful when exporting org file to html."
+  (interactive)
+  (let ((filename (if (equal major-mode 'dired-mode)
+  					  default-directory
+  					(buffer-file-name))))
+  	(if filename
+  		(progn
+  		  (kill-new filename)
+  		  (message "'%s' path copied!" filename))
+	  ;; print error message and exit/return the function
+  	  (user-error "Current buffer is not a file in disk!" "exit")))
+  (let ((pdf-file (concat (file-name-sans-extension (buffer-file-name)) ".pdf")))
+	(if (file-exists-p pdf-file)
+		(progn
+		  (kill-new pdf-file)
+		  (message "'%s' path copied!" pdf-file))
+	  (message "'%s' is not a file in disk!" pdf-file))))
 
 (defun insert-date-or-time (ARG)
   "Without prefix, print `2016-08-11'
@@ -1066,6 +1100,34 @@ Version 2016-07-13"
 		(fill-region -p1 -p2)))
 	(put this-command 'compact-p (not -compact-p))))
 (bind-keys* ("M-q" . xah-fill-or-unfill))
+
+(defun xah-syntax-color-hex ()
+  "Syntax color text of the form 「#ff1100」 and 「#abc」 in current buffer.
+URL `http://ergoemacs.org/emacs/emacs_CSS_colors.html'
+Version 2017-03-12"
+  (interactive)
+  (font-lock-add-keywords
+   nil
+   '(("#[[:xdigit:]]\\{3\\}"
+	  (0 (put-text-property
+		  (match-beginning 0)
+		  (match-end 0)
+		  'face (list :background
+					  (let* (
+							 (ms (match-string-no-properties 0))
+							 (r (substring ms 1 2))
+							 (g (substring ms 2 3))
+							 (b (substring ms 3 4)))
+						(concat "#" r r g g b b))))))
+	 ("#[[:xdigit:]]\\{6\\}"
+	  (0 (put-text-property
+		  (match-beginning 0)
+		  (match-end 0)
+		  'face (list :background (match-string-no-properties 0)))))))
+  (font-lock-flush))
+(add-hook 'prog-mode-hook 'xah-syntax-color-hex)
+(add-hook 'php-mode-hook 'xah-syntax-color-hex)
+(add-hook 'html-mode-hook 'xah-syntax-color-hex)
 
 ;; C-c e to 'show-ws-toggle-show-trailing-whitespace
 (defun cleanup-buffer ()
@@ -1700,11 +1762,14 @@ Emacs session."
 			(local-set-key (kbd "C-c o") 'ff-find-other-file)))
 
 ;; Saveplace & desktop
+;; https://www.emacswiki.org/emacs/SavePlace
+(setq save-place-file "~/.emacs.d/saved-places")
 (if (version< emacs-version "25.0")
 	(progn
 	  (require 'saveplace)
 	  (setq-default save-place t))
   (save-place-mode 1))
+
 ;;
 (setq desktop-save 'ask)
 ;;desktop-save ask means always ask
@@ -2032,9 +2097,9 @@ Do this after `q` in Debugger buffer."
 (setq package-archives
 	  '(
 		("gnu" . "http://elpa.gnu.org/packages/")
-		("melpa" . "https://melpa.org/packages/")
-		("melpa-stable" . "https://stable.melpa.org/packages/")
-		;; ("marmalade" . "https://marmalade-repo.org/packages/")
+		("melpa" . "http://melpa.org/packages/")
+		("melpa-stable" . "http://stable.melpa.org/packages/")
+		;; ("marmalade" . "http://marmalade-repo.org/packages/")
 		("ELPA" . "http://tromey.com/elpa/")
 		("user42" . "http://download.tuxfamily.org/user42/elpa/packages/")
 		))
@@ -2276,6 +2341,7 @@ Do this after `q` in Debugger buffer."
 				 (list
 				  (cons 'company-elisp
 						(car company-backends))))))
+(setq python-shell-prompt-detect-failure-warning nil)
 (add-hook 'python-mode-hook
 		  (lambda ()
 			(setq python-shell-interpreter "/usr/bin/python3.4")
@@ -2291,22 +2357,22 @@ Do this after `q` in Debugger buffer."
 							))))
 			(anaconda-mode)
 			(anaconda-eldoc-mode)
-			;; (setq python-shell-interpreter "python3.6")
-			(if (executable-find "ipython")
-				(progn (setq python-shell-interpreter "ipython")
-					   (if (version< (replace-regexp-in-string "\n$" "" (shell-command-to-string "ipython --version")) "5")
-						   (setq python-shell-interpreter-args "-i")
-						 (setq python-shell-interpreter-args "--simple-prompt -i")))
-			  (progn
-				(setq python-shell-interpreter-args "-i")
-				(setq python-shell-interpreter "python")))
 			;; solve the issue that line reaches the bottom of *iPython*, point
 			;; will will in the wrong place
 			(setq comint-scroll-to-bottom-on-input t)
-			(defadvice run-python (after run-python-buffer activate)
-			  "Switch to *Python* buffer after C-c C-p in a python buffer."
-			  (switch-to-buffer-other-window "*Python*"))
 			))
+;; use iPython inside Emacs directly once found when M-x run-python
+(if (executable-find "ipython")
+	(progn (setq python-shell-interpreter "ipython")
+		   (if (version< (replace-regexp-in-string "\n$" "" (shell-command-to-string "ipython --version")) "5")
+			   (setq python-shell-interpreter-args "-i")
+			 (setq python-shell-interpreter-args "--simple-prompt -i")))
+  (progn
+	(setq python-shell-interpreter-args "-i")
+	(setq python-shell-interpreter "python")))
+(defadvice run-python (after run-python-buffer activate)
+  "Switch to *Python* buffer after C-c C-p in a python buffer."
+  (switch-to-buffer-other-window "*Python*"))
 (defun my-python-tab-command (&optional _)
   "If the region is active, shift to the right; otherwise, indent current line.
 Indent the line/region according to the context which is smarter than default Tab/S-Tab"
@@ -2724,6 +2790,8 @@ background of code to whatever theme I'm using's background"
 ;; I would recommend any of the packages for an Emacs newbie.
 ;;
 ;; Icicles uses recursive Minibuffers in several ways, Helm does not.
+
+(setq org-highlight-latex-and-related '(latex script entities))
 
 ;; ;; ido for dired and kill-buffer that helm doesn't provide the TAB completion
 ;; (ido-mode t)
@@ -3376,7 +3444,9 @@ Version 2015-06-10"
 	 (bind-keys :map smartparens-mode-map
 				("M-<backspace>" . delete-word-backward)
 				("C-M-e" . nil)
-				("C-M-a" . nil))))
+				("C-M-a" . nil)
+				;; C-S-a redefined to beginning-of-visual-line
+				("C-S-a" . nil))))
 
 ;; deft
 (setq deft-directory "~/Org")
@@ -3401,7 +3471,7 @@ Version 2015-06-10"
  (cons "<" " < ")
  (cons ">" " > ")
  ;; (cons ";" "; ")
- (cons "++" " ++")
+ (cons "++" "++")
  )
 (electric-operator-add-rules-for-mode
  'c-mode
@@ -3409,7 +3479,7 @@ Version 2015-06-10"
  (cons "<" " < ")
  (cons ">" " > ")
  ;; (cons ";" "; ")
- (cons "++" " ++")
+ (cons "++" "++")
  )
 (electric-operator-add-rules-for-mode
  'org-mode
@@ -3464,157 +3534,7 @@ Version 2015-06-10"
  ;; (cons "o<--" " o<-- ")
  )
 
-;; LaTeX -- AucTex
-;; C-c ? (or M-x TeX-doc) gives documentation for the symbol at point,
-;; or for any package, command or document.
-(load "auctex.el" nil t t)
-(require 'tex-mik)
-(setq TeX-auto-save t)
-(setq TeX-parse-self t)
-;; when you open a file, ask for the master file but not changing any file
-;; this will make the LaTeX-save-and-compile deal with multi-files project
-(setq-default TeX-master 'shared)
-;; LaTeX-mode for tex file
-;; (add-hook 'plain-TeX-mode-hook 'LaTeX-mode)
-(add-to-list 'auto-mode-alist '("\\.tex$" . LaTeX-mode))
-;; company-auctex
-(require 'company-auctex)
-(require 'tex-buf)
-;; (require 'auctex-latexmk)
-;; (auctex-latexmk-setup)
-;;
-;; remove fdb_latexmk file will solve the problem
-;; of the reference number instead of question mark (?)
-(setq LaTeX-clean-intermediate-suffixes
-	  (append LaTeX-clean-intermediate-suffixes
-			  (list "\\.fdb_latexmk")))
-(defun LaTeX-save-and-compile ()
-  "Save and compile the tex project using latexmk.
-
-If compilation fails, split the current window and open error-buffer
-then jump to the error line, if errors corrected, close the error-buffer
-window and close the *TeX help* buffer."
-  (interactive)
-  (progn
-	;; ;; turn off smartparens because LaTeX-electric-left-right-brace
-	;; ;; offers more for specific LaTeX mode
-	;; ;; Since SP is always triggered later by sth., so put these two lines here
-	;; (turn-off-smartparens-mode)
-	;; (setq LaTeX-electric-left-right-brace t)
-	(let ((TeX-save-query nil)
-		  (TeX-process-asynchronous nil)
-		  (master-file (TeX-master-file)))
-	  (TeX-save-document "")
-	  ;; clean all generated files before compile
-	  ;; DO NOT do it when up-to-date, remove this line in proper time
-	  (TeX-clean t)
-	  (TeX-run-TeX "latexmk"
-				   (TeX-command-expand "latexmk -pdflatex='pdflatex -file-line-error -synctex=1' -pdf %s"
-									   'TeX-master-file)
-				   master-file)
-	  (if (plist-get TeX-error-report-switches (intern master-file))
-		  ;; avoid creating multiple windows to show the *TeX Help* error buffer
-		  (if (get-buffer-window (get-buffer "*TeX Help*"))
-			  (TeX-next-error)
-			(progn
-			  (split-window-vertically -10)
-			  (TeX-next-error)))
-		;; if no errors, delete *TeX Help* window and buffer
-		(if (get-buffer "*TeX Help*")
-			(progn
-			  (if (get-buffer-window (get-buffer "*TeX Help*"))
-				  (delete-windows-on "*TeX Help*"))
-			  (kill-buffer "*TeX Help*")))))))
-(add-hook 'LaTeX-mode-hook
-		  (lambda ()
-			(setq LaTeX-item-indent 0)
-			(visual-line-mode)
-			(flyspell-mode)
-			(setq-default TeX-newline-function 'advanced-return)
-			;; make the code look like the pdf file, C-c C-o ... for commands
-			;; If it should be activated in all AUCTEX modes, use TeX-mode-hook
-			;; instead of LaTeX-mode-hook.
-			(TeX-fold-mode 1)
-			;; usepackage
-			(setq tex-tree-roots t)
-			(LaTeX-math-mode)
-			;; this line have to be here to make company work
-			(company-auctex-init)
-			;; disable smartparens-mode completely and use
-			;; LaTeX-electric-left-right-brace instead
-			(push 'latex-mode sp-ignore-modes-list)
-			(setq LaTeX-electric-left-right-brace t)
-			;; the following line will inset braces after _ or ^
-			;; unnecessarily most of time
-			;; (setq TeX-electric-sub-and-superscript t)
-			;; NOTE: C-c C-a to combine C-c C-c and C-c C-v
-			;; C-u C-c C-c latexmk (or others like View) so you can change the command line
-			;; jump: the following makes viewing the pdf right at the line of the tex file
-			(add-to-list 'TeX-command-list
-						 '("latexmk" "latexmk -pdflatex='pdflatex -file-line-error -synctex=1' -pdf %s"
-						   TeX-run-command nil t :help "Run latexmk") t)
-			;; for tex file contains Chinese font
-			(add-to-list 'TeX-command-list
-						 '("XeLaTeX" "latexmk -pdflatex='xelatex -interaction nonstopmode -shell-escape' -pdf %s"
-						   TeX-run-command nil t :help "Run xelatex") t)
-			;; (setq TeX-command-default "XeLaTeX")
-			(setq TeX-command-default "latexmk")
-			(push '("%(masterdir)" (lambda nil (file-truename (TeX-master-directory))))
-				  TeX-expand-list)
-			;; Enable only one from all the 3 options to view pdf
-			;; Options1: Okular, the jumping to page may not work in old Okular
-			(push '("Okular" "okular --unique %o#src:%n%(masterdir)%b") TeX-view-program-list)
-			(push '(output-pdf "Okular") TeX-view-program-selection)
-			;; Options2: Evince
-			;; NOTE: it seems this options is builtin by default by AUCTEX
-			;; if you want to use this one, disable all of the three
-			;; (setq TeX-view-program-list '(("Evince" "evince --page-label=%(outpage) %o")))
-			;; (setq TeX-view-program-selection '((output-pdf "Evince")))
-			;; Options3: builtin pdf viewer, TODO: bug
-			;; (setq TeX-view-program-list '(("Emacs" "emacsclient %o")))
-			;; (setq TeX-view-program-selection '((output-pdf "Emacs")))
-			;;
-			(TeX-source-correlate-mode)
-			(server-force-delete)  ;; WARNING: Kills any existing edit server
-			(setq TeX-source-correlate-method 'synctex
-				  TeX-source-correlate-start-server t)
-			;;
-			(bind-keys :map LaTeX-mode-map
-					   ;; default C-c C-e rebound and cannot be rebound
-					   ("C-c C-x e" . LaTeX-environment)
-					   ("C-c C-x s" . LaTeX-section)
-					   ("C-c C-x m" . TeX-insert-macro)
-					   ("C-x C-s" . LaTeX-save-and-compile)
-					   ;; default C-c. not working and replaced by org-time-stamp
-					   ("C-c m" . LaTeX-mark-environment)
-					   ;; ("<tab>" . TeX-complete-symbol)
-					   ("M-<return>" . LaTeX-insert-item)
-					   )))
-(setq LaTeX-command-section-level t)
-;; C-c C-c without prompt, use Clean by default, to clean aux and log files
-;; Use "Clean All" to clean files including generated pdf file
-;; Or use M-x Tex-clean (Clean) and prefix(Clean All)
-;; (setq TeX-command-force "Clean")
-(setq TeX-clean-confirm nil)
-;; RefTex -- built-in
-;; Turn on RefTeX in AUCTeX
-(add-hook 'LaTeX-mode-hook 'turn-on-reftex)
-;; Activate nice interface between RefTeX and AUCTeX
-(setq reftex-plug-into-AUCTeX t)
-;; magic-latex-buffer
-;; (require 'magic-latex-buffer)
-;; (add-hook 'LaTeX-mode-hook 'magic-latex-buffer)
-(setq
- ;; Function for reading \includegraphics files
- LaTeX-includegraphics-read-file 'LaTeX-includegraphics-read-file-relative
- ;; Strip known extensions from image file name
- LaTeX-includegraphics-strip-extension-flag nil)
-;; (setq LaTeX-section-hook
-;;		  '(LaTeX-section-heading
-;;			LaTeX-section-title
-;;			LaTeX-section-toc
-;;			LaTeX-section-section
-;;			LaTeX-section-label))
+(require 'init-tex)
 
 ;; outline, C-c @ prefix
 (add-hook 'outline-minor-mode-hook
---
+++ lisp/init-tex.el
@@ -0,0 +1,154 @@
+;; LaTeX -- AucTex
+;; C-c ? (or M-x TeX-doc) gives documentation for the symbol at point,
+;; or for any package, command or document.
+(load "auctex.el" nil t t)
+(require 'tex-mik)
+(setq TeX-auto-save t)
+(setq TeX-parse-self t)
+;; when you open a file, ask for the master file but not changing any file
+;; this will make the LaTeX-save-and-compile deal with multi-files project
+(setq-default TeX-master 'shared)
+;; LaTeX-mode for tex file
+;; (add-hook 'plain-TeX-mode-hook 'LaTeX-mode)
+(add-to-list 'auto-mode-alist '("\\.tex$" . LaTeX-mode))
+;; company-auctex
+(require 'company-auctex)
+(require 'tex-buf)
+;; (require 'auctex-latexmk)
+;; (auctex-latexmk-setup)
+;;
+;; remove fdb_latexmk file will solve the problem
+;; of the reference number instead of question mark (?)
+(setq LaTeX-clean-intermediate-suffixes
+	  (append LaTeX-clean-intermediate-suffixes
+			  (list "\\.fdb_latexmk")))
+(defun LaTeX-save-and-compile ()
+  "Save and compile the tex project using latexmk.
+
+If compilation fails, split the current window and open error-buffer
+then jump to the error line, if errors corrected, close the error-buffer
+window and close the *TeX help* buffer."
+  (interactive)
+  (progn
+	;; ;; turn off smartparens because LaTeX-electric-left-right-brace
+	;; ;; offers more for specific LaTeX mode
+	;; ;; Since SP is always triggered later by sth., so put these two lines here
+	;; (turn-off-smartparens-mode)
+	;; (setq LaTeX-electric-left-right-brace t)
+	(let ((TeX-save-query nil)
+		  (TeX-process-asynchronous nil)
+		  (master-file (TeX-master-file)))
+	  (TeX-save-document "")
+	  ;; clean all generated files before compile
+	  ;; DO NOT do it when up-to-date, remove this line in proper time
+	  (TeX-clean t)
+	  (TeX-run-TeX "latexmk"
+				   (TeX-command-expand "latexmk -pdflatex='pdflatex -file-line-error -synctex=1' -pdf %s"
+									   'TeX-master-file)
+				   master-file)
+	  (if (plist-get TeX-error-report-switches (intern master-file))
+		  ;; avoid creating multiple windows to show the *TeX Help* error buffer
+		  (if (get-buffer-window (get-buffer "*TeX Help*"))
+			  (TeX-next-error)
+			(progn
+			  (split-window-vertically -10)
+			  (TeX-next-error)))
+		;; if no errors, delete *TeX Help* window and buffer
+		(if (get-buffer "*TeX Help*")
+			(progn
+			  (if (get-buffer-window (get-buffer "*TeX Help*"))
+				  (delete-windows-on "*TeX Help*"))
+			  (kill-buffer "*TeX Help*")))))))
+(add-hook 'LaTeX-mode-hook
+		  (lambda ()
+			(setq LaTeX-item-indent 0)
+			(visual-line-mode)
+			(flyspell-mode)
+			(setq-default TeX-newline-function 'advanced-return)
+			;; make the code look like the pdf file, C-c C-o ... for commands
+			;; If it should be activated in all AUCTEX modes, use TeX-mode-hook
+			;; instead of LaTeX-mode-hook.
+			(TeX-fold-mode 1)
+			;; usepackage
+			(setq tex-tree-roots t)
+			(LaTeX-math-mode)
+			;; this line have to be here to make company work
+			(company-auctex-init)
+			;; disable smartparens-mode completely and use
+			;; LaTeX-electric-left-right-brace instead
+			(push 'latex-mode sp-ignore-modes-list)
+			(setq LaTeX-electric-left-right-brace t)
+			;; the following line will inset braces after _ or ^
+			;; unnecessarily most of time
+			;; (setq TeX-electric-sub-and-superscript t)
+			;; NOTE: C-c C-a to combine C-c C-c and C-c C-v
+			;; C-u C-c C-c latexmk (or others like View) so you can change the command line
+			;; jump: the following makes viewing the pdf right at the line of the tex file
+			(add-to-list 'TeX-command-list
+						 '("latexmk" "latexmk -pdflatex='pdflatex -file-line-error -synctex=1' -pdf %s"
+						   TeX-run-command nil t :help "Run latexmk") t)
+			;; for tex file contains Chinese font
+			(add-to-list 'TeX-command-list
+						 '("XeLaTeX" "latexmk -pdflatex='xelatex -interaction nonstopmode -shell-escape' -pdf %s"
+						   TeX-run-command nil t :help "Run xelatex") t)
+			;; (setq TeX-command-default "XeLaTeX")
+			(setq TeX-command-default "latexmk")
+			(push '("%(masterdir)" (lambda nil (file-truename (TeX-master-directory))))
+				  TeX-expand-list)
+			;; Enable only one from all the 3 options to view pdf
+			;; Options1: Okular, the jumping to page may not work in old Okular
+			(push '("Okular" "okular --unique %o#src:%n%(masterdir)%b") TeX-view-program-list)
+			(push '(output-pdf "Okular") TeX-view-program-selection)
+			;; Options2: Evince
+			;; NOTE: it seems this options is builtin by default by AUCTEX
+			;; if you want to use this one, disable all of the three
+			;; (setq TeX-view-program-list '(("Evince" "evince --page-label=%(outpage) %o")))
+			;; (setq TeX-view-program-selection '((output-pdf "Evince")))
+			;; Options3: builtin pdf viewer, TODO: bug
+			;; (setq TeX-view-program-list '(("Emacs" "emacsclient %o")))
+			;; (setq TeX-view-program-selection '((output-pdf "Emacs")))
+			;;
+			(TeX-source-correlate-mode)
+			(server-force-delete)  ;; WARNING: Kills any existing edit server
+			(setq TeX-source-correlate-method 'synctex
+				  TeX-source-correlate-start-server t)
+			;;
+			(bind-keys :map LaTeX-mode-map
+					   ;; default C-c C-e rebound and cannot be rebound
+					   ("C-c C-x e" . LaTeX-environment)
+					   ("C-c C-x s" . LaTeX-section)
+					   ("C-c C-x m" . TeX-insert-macro)
+					   ("C-x C-s" . LaTeX-save-and-compile)
+					   ;; default C-c. not working and replaced by org-time-stamp
+					   ("C-c m" . LaTeX-mark-environment)
+					   ;; ("<tab>" . TeX-complete-symbol)
+					   ("M-<return>" . LaTeX-insert-item)
+					   )))
+(setq LaTeX-command-section-level t)
+;; C-c C-c without prompt, use Clean by default, to clean aux and log files
+;; Use "Clean All" to clean files including generated pdf file
+;; Or use M-x Tex-clean (Clean) and prefix(Clean All)
+;; (setq TeX-command-force "Clean")
+(setq TeX-clean-confirm nil)
+;; RefTex -- built-in
+;; Turn on RefTeX in AUCTeX
+(add-hook 'LaTeX-mode-hook 'turn-on-reftex)
+;; Activate nice interface between RefTeX and AUCTeX
+(setq reftex-plug-into-AUCTeX t)
+;; magic-latex-buffer
+;; (require 'magic-latex-buffer)
+;; (add-hook 'LaTeX-mode-hook 'magic-latex-buffer)
+(setq
+ ;; Function for reading \includegraphics files
+ LaTeX-includegraphics-read-file 'LaTeX-includegraphics-read-file-relative
+ ;; Strip known extensions from image file name
+ LaTeX-includegraphics-strip-extension-flag nil)
+;; (setq LaTeX-section-hook
+;;		  '(LaTeX-section-heading
+;;			LaTeX-section-title
+;;			LaTeX-section-toc
+;;			LaTeX-section-section
+;;			LaTeX-section-label))
+
+
+(provide 'init-tex)
