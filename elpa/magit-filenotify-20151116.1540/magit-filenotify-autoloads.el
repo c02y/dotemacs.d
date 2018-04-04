ac3f133ccc92e14333035a6862bfa9d2b8fdc85e
c02y <cody.chan.cz@gmail.com>
2018-04-04 19:55:57 +0800

    elpa: big upgrade, upgrade magit from old magit-90150116(2014) to magit-20180403

--- elpa/magit-filenotify-20151116.1540/magit-filenotify-autoloads.el
+++ elpa/magit-filenotify-20151116.1540/magit-filenotify-autoloads.el
@@ -1,10 +1,10 @@
 ;;; magit-filenotify-autoloads.el --- automatically extracted autoloads
 ;;
 ;;; Code:
-(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
+(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
 
-;;;### (autoloads nil "magit-filenotify" "magit-filenotify.el" (22091
-;;;;;;  29944 726582 315000))
+;;;### (autoloads nil "magit-filenotify" "magit-filenotify.el" (23236
+;;;;;;  48381 914994 870000))
 ;;; Generated autoloads from magit-filenotify.el
 
 (autoload 'magit-filenotify-mode "magit-filenotify" "\
