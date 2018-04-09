9ec44bf20d6ff6fc4fe94367d53eb1849e950873
c02y <cody.chan.cz@gmail.com>
2018-04-09 23:54:14 +0800

    elpa: updates

---
+++ elpa/highlight-indent-guides-20180404.1216/highlight-indent-guides-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "highlight-indent-guides" "20180404.1216" "Minor mode to highlight indentation" '((emacs "24")) :stars '(#("74" 0 2 (font-lock-face paradox-star-face))) :commit "a968436711520d4728c0b6c64279eb5eae7e0e5d" :url "https://github.com/DarthFennec/highlight-indent-guides")
