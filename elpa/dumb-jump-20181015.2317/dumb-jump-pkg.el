c1f5797438bd1230f706362938c2d94718d3d4ad
c02y <cody.chan.cz@gmail.com>
2018-11-25 19:01:02 +0800

    elpa: updates, install easy-kill package

--- elpa/dumb-jump-20181015.2317/dumb-jump-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "dumb-jump" "20181015.2317" "jump to definition for multiple languages without configuration." '((emacs "24.3") (f "0.20.0") (s "1.11.0") (dash "2.9.0") (popup "0.5.3")) :commit "b9692be3f7541092dd06c58935e168fc9f881490" :keywords '("programming") :authors '(("jack angers")) :maintainer '("jack angers"))
---
+++ elpa/swiper-20181120.1832/swiper-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "swiper" "20181120.1832" "Isearch with an overview. Oh, man!" '((emacs "24.1") (ivy "0.9.0")) :commit "79543f3e61a67f2830f88e96fe11d741b495c0f7" :keywords '("matching") :authors '(("Oleh Krehel" . "ohwoeowho@gmail.com")) :maintainer '("Oleh Krehel" . "ohwoeowho@gmail.com") :url "https://github.com/abo-abo/swiper")
