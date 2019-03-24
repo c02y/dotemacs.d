b0e2d4a9b886dff31255cd5204043a2393fdb6b2
c02y <cody.chan.cz@gmail.com>
2019-03-24 20:57:15 +0800

    elpa: updates

---
+++ elpa/git-commit-20190319.2352/git-commit-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "git-commit" "20190319.2352" "Edit Git commit messages" '((emacs "25.1") (dash "20180910") (with-editor "20181103")) :commit "217f1a7da05778418a45472115845b4ab3adaf5d" :keywords '("git" "tools" "vc") :maintainer '("Jonas Bernoulli" . "jonas@bernoul.li") :url "https://github.com/magit/magit")
