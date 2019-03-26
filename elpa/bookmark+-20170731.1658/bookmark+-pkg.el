8d4158f526580968872e780d7da4e1cdef4f6eb1
c02y <cody.chan.cz@gmail.com>
2019-03-26 22:03:56 +0800

    elpa: remove dired+/bookmark+ packages

--- elpa/bookmark+-20170731.1658/bookmark+-pkg.el
+++
@@ -1,5 +0,0 @@
-(define-package "bookmark+" "20170731.1658" "Bookmark+: extensions to standard library `bookmark.el'." 'nil :url "https://www.emacswiki.org/emacs/download/bookmark%2b.el" :keywords
-  '("bookmarks" "bookmark+" "projects" "placeholders" "annotations" "search" "info" "url" "eww" "w3m" "gnus"))
-;; Local Variables:
-;; no-byte-compile: t
-;; End:
